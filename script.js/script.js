aaf803094332c0a93843a77ac25b7707f0a29961
Omareo Wells <75456221+Owells21@users.noreply.github.com>
2021-10-18 23:01:59 -0400

    Add files via upload

---
+++ script.js/script.js
@@ -0,0 +1,12 @@
+// This  function clear all the values 
+function clearScreen() {
+    document.getElementById("result").value ="";
+}
+
+//This function evaluates the expression and return result
+
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
